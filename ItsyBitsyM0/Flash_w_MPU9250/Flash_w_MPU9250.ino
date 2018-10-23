ca7ebeaad12c917dbcec90b7c94376deffd69318
Sean <seanpmcneill@gmail.com>
2018-10-23 12:53:02 -0400

    Added RTC to MPU9250 Flash

--- ItsyBitsyM0/Flash_w_MPU9250/Flash_w_MPU9250.ino
+++ ItsyBitsyM0/Flash_w_MPU9250/Flash_w_MPU9250.ino
@@ -2,6 +2,7 @@
 #include <Adafruit_SPIFlash.h>
 #include <Adafruit_SPIFlash_FatFs.h>
 #include <SparkFunMPU9250-DMP.h>
+#include <RTCZero.h>
 
 // Include the FatFs library header to use its low level functions
 // directly.  Specifically the f_fdisk and f_mkfs functions are used
@@ -27,8 +28,12 @@ unsigned int count = 0;
 unsigned int count_max = 5;
 unsigned int total = 0;
 unsigned int total_max = 2;
+unsigned long startTime = 0;
+unsigned long endTime = 0;
+
 
 MPU9250_DMP imu;
+RTCZero rtc;
 
 void setup() {
   // Initialize serial port and wait for it to open before continuing.
@@ -37,20 +42,27 @@ void setup() {
     delay(100);
   }
 
+  // Set up the real-time counter
+  rtc.begin(); // initialize rtc
+  rtc.setTime(0, 0, 0);
+  rtc.setDate(0, 0, 0);
+  printTime();
+  startTime = millis();
+
   // format and mount flash file system
-  if(format()) Serial.println("\n\nFormatting has been stopped...");
+  if (format()) Serial.println("\n\nFormatting has been stopped...");
 
   // ask if IMU is present
-  while(1)
+  while (1)
   {
     Serial.println("Do you have an IMU present? (Y/N)");
-    while(Serial.available() == 0) delay(100);
-    if(Serial.available() > 0)
+    while (Serial.available() == 0) delay(100);
+    if (Serial.available() > 0)
     {
       char a = Serial.read();
-      if(a == 'Y') imu_present = true;
+      if (a == 'Y') imu_present = true;
       else if (a == 'N') imu_present = false;
-      else 
+      else
       {
         Serial.println("Input not accepted, try again ...");
         continue;
@@ -59,22 +71,22 @@ void setup() {
     }
   }
 
-  
-  if(imu_present)
+
+  if (imu_present)
   {
     // Call imu.begin() to verify communication with and
     // initialize the MPU-9250 to it's default values.
     // Most functions return an error code - INV_SUCCESS (0)
     // indicates the IMU was present and successfully set up
-    
+
     Serial.print("Turning on IMU...");
-    if(imu.begin() != INV_SUCCESS)
+    if (imu.begin() != INV_SUCCESS)
     {
       Serial.println("There has been an error initializing the IMU");
-      while(1) delay(100);
+      while (1) delay(100);
     }
     else Serial.println(" Done!");
-    
+
     Serial.print("Enabling sensors ");
     // Use setSensors to turn on or off MPU-9250 sensors.
     // Any of the following defines can be combined:
@@ -91,19 +103,19 @@ void setup() {
     // Accel options are +/- 2, 4, 8, or 16 g
     imu.setAccelFSR(2); // Set accel to +/-2g
     Serial.println("... Accel Done! ");
-    // Note: the MPU-9250's magnetometer FSR is set at 
+    // Note: the MPU-9250's magnetometer FSR is set at
     // +/- 4912 uT (micro-tesla's)
-  
+
     // setLPF() can be used to set the digital low-pass filter
     // of the accelerometer and gyroscope.
     // Can be any of the following: 188, 98, 42, 20, 10, 5
     // (values are in Hz).
     imu.setLPF(5); // Set LPF corner frequency to 5Hz
-  
+
     // The sample rate of the accel/gyro can be set using
     // setSampleRate. Acceptable values range from 4Hz to 1kHz
     imu.setSampleRate(10); // Set sample rate to 10Hz
-  
+
     // Likewise, the compass (magnetometer) sample rate can be
     // set using the setCompassSampleRate() function.
     // This value can range between: 1-100Hz
@@ -112,75 +124,71 @@ void setup() {
   }
 
   else Serial.println("Sensor readings will be assigned random numbers.");
+
+  // Write headers to file
+  File dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
+  if (dataFile)
+  {
+    dataFile.println("Time,Ax,Ay,Az");
+    dataFile.close();
+  }
 }
 
 void loop() {
 
   // Stop storing/reading data after a certain number of reads
-  
-  if(total > total_max)
+
+  if (total > total_max)
   {
+    endTime = millis();
     Serial.println("\nCompleted sensor storage/reading");
-    while(1) delay(100);
+    printTime();
+    Serial.println(endTime - startTime);
+    while (1) delay(100);
   }
 
- 
-  else{
 
-    // After storing 5 sensor readings, 
+  else {
+
+    // After storing 5 sensor readings,
     // read the file to verify correct operation
-    
-    if(++count > 5)
+    if (++count > 5)
     {
       total++;
       count = 0;
-      File dataFile = fatfs.open(FILE_NAME, FILE_READ);
-      if (!dataFile) {
-        Serial.println("Error, failed to open file for reading!");
-        while(1);
-      }
-  
-      // And finally to read all the data and print it out a character at a time
-      // (stopping when end of file is reached):
-      Serial.println("\nEntire contents of file:");
-      while (dataFile.available()) {
-        char c = dataFile.read();
-        Serial.print(c);
-      }
-
-      Serial.print("Total size of file (bytes): ");
-      Serial.print(dataFile.size());
-      Serial.print(" (");
-      Serial.print((float)(dataFile.size())/(1024*1024),DEC);
-      Serial.println(" MB)");
-      Serial.println();
-
-      dataFile.close();
+      printFileContents(FILE_NAME);
     }
-    
-    else{
+
+    else {
 
       // Read sensor and write sensor data to file
       File dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
       if (dataFile) {
         int16_t accelX, accelY, accelZ;
-        if(imu_present && imu.dataReady())
+        if (imu_present && imu.dataReady())
         {
           imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
           accelX = imu.ax;
           accelY = imu.ay;
-          accelZ = imu.az; 
+          accelZ = imu.az;
         }
         else
         {
-          accelX = random(-100,100);
-          accelY = random(-100,100);
-          accelZ = random(-100,100);           
+          accelX = random(-100, 100);
+          accelY = random(-100, 100);
+          accelZ = random(-100, 100);
         }
 
-        // Write a line to the file. 
+        // Write a line to the file.
         // Write comma separated values
-        dataFile.print("Accel Reading");
+        if (rtc.getHours() < 10) dataFile.print("0");
+        dataFile.print(rtc.getHours());
+        dataFile.print(":");
+        if (rtc.getMinutes() < 10) dataFile.print("0");
+        dataFile.print(rtc.getMinutes());
+        dataFile.print(":");
+        if (rtc.getSeconds() < 10) dataFile.print("0");
+        dataFile.print(rtc.getSeconds());
         dataFile.print(",");
         dataFile.print(accelX, DEC);
         dataFile.print(",");
@@ -188,7 +196,7 @@ void loop() {
         dataFile.print(",");
         dataFile.print(accelZ, DEC);
         dataFile.println();
-        
+
         // Finally close the file when done writing.  This is smart to do to make
         // sure all the data is written to the file.
         dataFile.close();
@@ -199,18 +207,18 @@ void loop() {
         Serial.println("Failed to open data file for writing!");
       }
     }
-    
+
     // Wait 1 seconds.
     delay(1000L);
   }
 }
 
-int format(){
-  
+int format() {
+
   // Initialize flash library and check its chip ID.
   if (!flash.begin(FLASH_TYPE)) {
     Serial.println("Error, failed to initialize flash chip!");
-    while(1);
+    while (1);
   }
   Serial.print("Flash chip JEDEC ID: 0x"); Serial.println(flash.GetJEDECID(), HEX);
   Serial.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -222,12 +230,12 @@ int format(){
   Serial.setTimeout(1000);
 
   int countdown = WAIT;
-  while(countdown){
-    if(Serial.find("STOP")) return 1;
+  while (countdown) {
+    if (Serial.find("STOP")) return 1;
     Serial.print(countdown--);
     Serial.print("... ");
   }
-  
+
   fatfs.activate();
 
   // Partition the flash with 1 partition that takes the entire space.
@@ -237,7 +245,7 @@ int format(){
   FRESULT r = f_fdisk(0, plist, buf);
   if (r != FR_OK) {
     Serial.print("Error, f_fdisk failed with error code: "); Serial.println(r, DEC);
-    while(1);
+    while (1);
   }
   Serial.println("Partitioned flash!");
 
@@ -246,16 +254,55 @@ int format(){
   r = f_mkfs("", FM_ANY, 0, buf, sizeof(buf));
   if (r != FR_OK) {
     Serial.print("Error, f_mkfs failed with error code: "); Serial.println(r, DEC);
-    while(1);
+    while (1);
   }
   Serial.println("Formatted flash!");
 
   // Test that the file system is mounted
   if (!fatfs.begin()) {
     Serial.println("Error, failed to mount newly formatted filesystem!");
-    while(1);
+    while (1);
   }
   Serial.println("Flash chip successfully formatted with new empty filesystem!");
 
   return 0;
+}
+
+void printTime() {
+  // Prints time in HH:MM:SS format
+  Serial.print("Time: ");
+  if (rtc.getHours() < 10) Serial.print("0");
+  Serial.print(rtc.getHours());
+  Serial.print(":");
+  if (rtc.getMinutes() < 10) Serial.print("0");
+  Serial.print(rtc.getMinutes());
+  Serial.print(":");
+  if (rtc.getSeconds() < 10) Serial.print("0");
+  Serial.println(rtc.getSeconds());
+}
+
+void printFileContents(String fileName) {
+  File dataFile = fatfs.open(fileName, FILE_READ);
+  if (!dataFile) {
+    Serial.println("Error, failed to open file for reading!");
+    while (1);
+  }
+
+  // read all the data and print it out a character at a time
+  // (stopping when end of file is reached):
+  Serial.println("\nEntire contents of file:");
+  while (dataFile.available()) {
+    char c = dataFile.read();
+    Serial.print(c);
+  }
+
+  // Print the total size of file
+  Serial.print("Total size of file (bytes): ");
+  Serial.print(dataFile.size());
+  Serial.print(" (");
+  Serial.print((float)(dataFile.size()) / (1024 * 1024), DEC);
+  Serial.println(" MB)");
+  Serial.println();
+
+  dataFile.close();
 }
